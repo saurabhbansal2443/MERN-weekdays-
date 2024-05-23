945bf47df35d43379065483fe49ecf641d8c3c8f
Saurabh Bansal <saurabhbansal2443@gmail.com>
2024-05-23 19:24:11 +0530

    search function added

--- React/first-project/src/Home.jsx
+++ React/first-project/src/Home.jsx
@@ -4,34 +4,70 @@ import { useState } from "react";
 
 let Home = () => {
   let [ProductArray, setProductArray] = useState([...ProductData]);
+  let [searchText , setSearchText] = useState("");
 
   // let topRatedData =[...ProductData]
 
   let filterToprated = () => {
-    let data = ProductArray.filter((obj) => {
+    let data = ProductData.filter((obj) => {
       return obj.rating > 4.5;
     });
 
     setProductArray(data);
   };
 
-  console.log(ProductArray);
+  let filterProduct=(proCategory)=>{
+    let data = ProductData.filter((obj) => {
+        return proCategory == obj.category
+      });
+  
+      setProductArray(data);
+  }
+
+  let searchProduct = ()=>{
+    let data = ProductData.filter((obj) => obj.title.toLowerCase().includes(searchText.toLowerCase()) );
+
+   
+  
+      setProductArray(data);
+
+      setSearchText("")
+  }
+
+//    console.log("Home page");
 
   return (
-    <div className="border-2 border-red-600  min-h-[93.7vh] ">
+    <div className="border-2 border-red-600  min-h-[93.7vh] bg-white text-2xl ">
       <div className="utility flex  justify-center m-5">
-        <div className="search-box w-1/2">
+       
+        <div className=" flex justify-around  w-full">
+          <button className="btn btn-xs text-2xl sm:btn-sm md:btn-md lg:btn-lg" onClick={filterToprated}>
+            Top Rated
+          </button>
+          <button className="btn btn-xs text-2xl sm:btn-sm md:btn-md lg:btn-lg" onClick={()=>filterProduct("smartphones")}>
+           SmartPhones 
+          </button>
+
+{/* Serach button functions  */}
+          <div className="search-box  flex ">
           <input
+              value = {searchText}
+             onChange={ (event)=>setSearchText(event.target.value) }
             type="text"
             placeholder="Type here"
-            className="input input-bordered w-full max-w-xs mx-3"
+            className="input input-bordered w-full max-w-xs mx-3 bg-white text-black text-2xl border-2 border-black"
           />
-          <button className="btn btn-active btn-accent">Accent</button>
+          <button className="btn btn-active btn-accent text-2xl" onClick={searchProduct}>Search </button>
         </div>
-        <div className="btn">
-          <button className="btn btn-wide text-2xl" onClick={filterToprated}>
-            Top Rated{" "}
+
+
+          <button className="btn btn-xs text-2xl sm:btn-sm md:btn-md lg:btn-lg" onClick={()=>filterProduct("laptops")}>
+            Laptop
           </button>
+       <button className="btn btn-xs text-2xl sm:btn-sm md:btn-md lg:btn-lg" onClick={()=>filterProduct("skincare")}>
+            SkinCare 
+          </button> 
+          
         </div>
       </div>
       <div className="cards flex flex-wrap justify-around">
--- React/first-project/src/ProductCard.jsx
+++ React/first-project/src/ProductCard.jsx
@@ -1,20 +1,20 @@
 let ProductCard = ({ obj }) => {
   let { images, title, description, rating, category, price } = obj;
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src={images[images.length-1]} alt="Shoes" /></figure>
-  <div className="card-body">
+    <div className="card h-[60vh] w-96 bg-base-100 shadow-xl border-2 m-4 border-red-600">
+  <figure className="h-1/2"><img src={images[images.length-1]} alt="Shoes" /></figure>
+  <div className="card-body h-1/2">
     <h2 className="card-title">
       {title}  
       <div className="badge badge-secondary">{rating}</div>
-    <div className="badge badge-secondary">{category}</div>
+    <div className="badge badge-secondary text-wrap">{category}</div>
     </h2>
    
-    <p>{description} </p>
+    <p className="h-1/6 overflow-scroll">{description} </p>
     <div className="card-actions justify-end">
       {/* <div className="badge badge-outline">Fashion</div> 
       <div className="badge badge-outline">Products</div> */}
-      <button className="btn btn-active btn-primary">{price}</button>
+      <p className="text-2xl text-red-500"> Rs {price }</p>
       <button className="btn btn-active btn-primary">Add to Cart </button>
     </div>
   </div>
